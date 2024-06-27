989e2d627cab0b42a3024c4578323512371c4650
fiona <fiona.mburu@student.moringaschool.com>
2024-06-27 15:33:54 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10)
 
-// Write your code here
+let num3 =19;
+let num4 =5;
+let mod = num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(5, 10, 15, 20);
+
+
+
+
+  
